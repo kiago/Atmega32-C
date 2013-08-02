db6e3b28274c27be3ffec303ab3918f3e60220ed
kiago <kikiago@gmail.com>
2013-08-02 21:28:16 +1000

    LED=switches

--- firmware/main.c
+++ firmware/main.c
@@ -1,21 +1,24 @@
-/* Name: main.c
- * Author: <insert your name here>
- * Copyright: <insert your copyright message here>
- * License: <insert your license reference here>
- */
+// C-program ATmega32 : OPENUSB
+//  LED on/off
+// with SWITCHES
+// 
 
-#include <avr/io.h>
-#include <util/delay.h>
-int main(void)
-{
-    DDRB=0xFF; //all pins of PORTB declared as output
-    PORTB=0x00;
-    while(1)
-    {
-        //TODO::
-        PORTB=0xFF;  //High State
-        _delay_ms(200); //delay
-        PORTB=0x00;
-        _delay_ms(200); //delay
-    }
+#include <avr/io.h>     // avr header file for IO ports
+#include <stdio.h>
+
+int main(void){
+    unsigned char i; // temporary variable
+    
+    DDRC = 0x00;    // set PORTC for input
+    DDRB = 0xFF;    // set PORTB for output
+    
+    PORTB = 0x00;   // turn ON all LEDs initially
+    PORTC = 0XFF;   //
+    while(1){
+        // Send output to PORTB.
+        // This port will be connected to the 8 LEDs
+        i = PINC; // i take switches data
+        PORTB = i;  //and LEDs data take i !
+    } 
+    return 1; 
 }
